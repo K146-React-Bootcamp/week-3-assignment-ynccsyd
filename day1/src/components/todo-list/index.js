3f31f19871c109379fe4a51d17a4376e633208c1
ynccsyd <muhibbi_10_@hotmaile.com>
2022-10-08 01:01:22 +0300

    Added

--- day1/src/components/todo-list/index.js
+++ day1/src/components/todo-list/index.js
@@ -6,13 +6,19 @@ const url = "https://jsonplaceholder.typicode.com/todos";
 
 const TodoList = () => {
 	const [todos, setTodos] = useState([]);
-  const [selectedTodo, setSelectedTodo] = useState();
+	const [selectedTodo, setSelectedTodo] = useState();
+	const [totalPage, setTotalPage] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [changeValue, setChangeValue] =useState('');
+
 
 	useEffect(() => {
 		fetch(url)
 			.then((response) => response.json())
 			.then((todos) => {
 				setTodos(todos);
+				setTotalPage(Math.ceil(todos.length/15));
+		
 			})
 			.catch((err) => {
 				console.log(err);
@@ -33,22 +39,40 @@ const TodoList = () => {
 	};
 
 
-  const remove = (todo) => {
-    if (window.confirm("Silmek üzerisiniz emin misiniz")) {
-      setTodos(prev => {
-        return prev.filter(x => x.id != todo.id)
-      })
-    }
-  }
+	const remove = (todo) => {
+		if (window.confirm("Silmek üzerisiniz emin misiniz")) {
+			setTodos(prev => {
+				return prev.filter(x => x.id !== todo.id)
+			})
+		}
+	};
 
-  const edit = (todo) => {
-    setSelectedTodo(todo);
-  }
+	const edit = (todo) => {
+		setSelectedTodo(todo);
+	};
+
+
+	const handleKaydet = () => {
+		const newTodos = todos.map((todo) => {
+			if (todo.id === selectedTodo.id) {
+				return { ...todo, title: changeValue };
+			} else {
+				return todo;
+			}
+		});
+		setTodos(newTodos);
+	};
+
+
+	const getDataWithPage = (page) => {
+		return todos.slice((page - 1) * 10, page * 10);
+	};
+
 
 	const renderBody = () => {
 		return (
 			<tbody>
-				{todos.slice(0, 15).map((todo, index) => {
+				{getDataWithPage(currentPage).map((todo, index) => {
 					return (
 						<tr key={index}>
 							<td>{todo.id}</td>
@@ -71,23 +95,47 @@ const TodoList = () => {
 	};
 
 
-  const renderEditForm = () => {
-    return (
-      <div>
-        <input type={"text"}/>
-        <inpu type="check" />
-        <Button>Kaydet</Button>
-        <Button onClick={() => setSelectedTodo(undefined)}>Vazgeç</Button>
-      </div>
-    )
-  }
+	const renderEditForm = () => {
+		return (
+			<div>
+				<input type={"text"} onChange={(e) => setChangeValue(e.target.value)} />
+				<inpu type="check" />
+				<Button onClick={handleKaydet}>Kaydet</Button>
+				<Button onClick={() => setSelectedTodo(undefined)}>Vazgeç</Button>
+			</div>
+		)
+	};
+
+
+
+
 	return (
-    <div className={`${classes.container} container`}>
-      { selectedTodo && renderEditForm()}
+		<div className={`${classes.container} container`}>
+			{selectedTodo && renderEditForm()}
 			<table className="table">
 				{renderThead()}
-        {renderBody()}
+				{renderBody()}
 			</table>
+			<div className="d-flex justify-content-center">
+				<Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1} >
+					Prev
+				</Button>
+				{Array.from({ length: totalPage }, (v, i) => i  + 1).map((page) => (
+					<Button className = { page === currentPage ? 'btn btn-primary' : 'btn btn-outline-primary'}
+						onClick={() => setCurrentPage(page)}
+					>
+						{page}
+
+					</Button>
+
+				))}
+
+				<Button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPage}>
+					Next
+				</Button>
+
+			</div>
+
 		</div>
 	);
 };
